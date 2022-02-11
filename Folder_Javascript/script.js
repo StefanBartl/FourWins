609d99ab5027158a1dc91f1b8208aba350e34d53
Stefan Bartl <bartl.stefan@me.com>
2022-02-11 05:17:58 +0100

    First version with basic functionality and setup

--- Folder_Javascript/script.js
+++ Folder_Javascript/script.js
@@ -1,73 +1,173 @@
-let column_1_Counter = 0;
+ /*                              _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+                                |#                                                       #|
+                                |                 Table of content:                       |
+                                |                                                         |
+                                |                    1)                            |
+                                |                    2)                           |
+                                |                    3)                        |
+                                |                    4)            |
+                                |                    5)                         |
+                                |                    6)                      |
+                                |                    7)                            |
+                                |                                                         |
+                                |#_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _#|
+
+
+/*                                                       General                                                         */
+
+
+
+console.log();
+
+// Setting the Counters for knowing in wich row of each column the game currently is
+
+let column_1_Counter = 8;
+let column_2_Counter = 8;
+let column_3_Counter = 8;
+let column_4_Counter = 8;
+let column_5_Counter = 8;
+let column_6_Counter = 8;
+let column_7_Counter = 8;
+
+// Setting variable to know who is on turn
+let playerIsOnTurn = "left";
+
 // Get the Top Cells for looping trough
+
 const topCellsArray = document.getElementsByClassName("Class_TopCells");
+
+// Main Game (for-of-)-Loop
+
 for(let topCell of topCellsArray){
+
 // Adding & Removing the "Choose the Column" Animation just by adding the Class with the CSS-Animation
-topCell.addEventListener("mouseover", ()=>{ topCell.classList.add("Class_ChoosingAnimation")});
-topCell.addEventListener("mouseleave", ()=>{ topCell.classList.remove("Class_ChoosingAnimation")});
-topCell.addEventListener("click", ()=>{ topCell.classList.remove("Class_ChoosingAnimation")})
 
+topCell.addEventListener("mouseover", ()=>{ 
+    if(playerIsOnTurn === "left") topCell.classList.add("Class_ChoosingAnimation_Coin_2")
+    else topCell.classList.add("Class_ChoosingAnimation_Coin_1")
+});
+topCell.addEventListener("mouseleave", ()=>{ 
+    if(playerIsOnTurn === "left") topCell.classList.remove("Class_ChoosingAnimation_Coin_2")
+    else topCell.classList.remove("Class_ChoosingAnimation_Coin_1")
+});
 
+topCell.addEventListener("click", ()=>{ 
+    
+    if(playerIsOnTurn === "left") topCell.classList.remove("Class_ChoosingAnimation_Coin_2")
+    else topCell.classList.remove("Class_ChoosingAnimation_Coin_1")
+
+    if(playerIsOnTurn === "left")playerIsOnTurn = "right"
+    else if(playerIsOnTurn === "right")playerIsOnTurn = "left";
+});
 
 // Adding the "Coin fly trough the Gameboard" animation
-topCell.addEventListener("click", ()=>{
-// Get the correct cells of the column
+
+topCell.addEventListener("click", function GameFlow (){
+
+// Get the correct rows of each cell in the column of the chosen top cell
+// First get the column number via the id of the top cell
+
 let topCellID = topCell.getAttribute("id");
-let cell_2 = document.getElementById(`ID_C${topCellID[4]}R2`);
-let cell_3 = document.getElementById(`ID_C${topCellID[4]}R3`);
-let cell_4 = document.getElementById(`ID_C${topCellID[4]}R4`);
-let cell_5 = document.getElementById(`ID_C${topCellID[4]}R5`);
-let cell_6 = document.getElementById(`ID_C${topCellID[4]}R6`);
-let cell_7 = document.getElementById(`ID_C${topCellID[4]}R7`);
+let columnNumber = parseInt(topCellID[4]);
 
-animater(topCell, topCellID[4]);
+// Next invoke the Main Game function which must stay inside the event listener, because of the later 
+// neccesary removing of an eventassign the CSS-Animations & DOM-Manipulations
+// Giving the second parameter to control the CSS-Animations & DOM-Manipulations (f.e. getting the correct rows)
 
+Main_Game_Function(topCell, columnNumber);
 
+// Main Game Function
 
+function Main_Game_Function(cell, columnNumber){
+console.log(playerIsOnTurn);
+// console.log(" Played cell: " + cell.id + " in the " + columnNumber + " column.");
+// Decrease the row counter to count the columns played in this row and setting a variable for 
+// the correct animations and placement (to get the correct correct column)
 
+let animationVar;
+     if(columnNumber === 1) {column_1_Counter--; animationVar = column_1_Counter;}
+else if(columnNumber === 2) {column_2_Counter--; animationVar = column_2_Counter;}
+else if(columnNumber === 3) {column_3_Counter--; animationVar = column_3_Counter;}
+else if(columnNumber === 4) {column_4_Counter--; animationVar = column_4_Counter;}
+else if(columnNumber === 5) {column_5_Counter--; animationVar = column_5_Counter;}
+else if(columnNumber === 6) {column_6_Counter--; animationVar = column_6_Counter;}
+else if(columnNumber === 7) {column_7_Counter--; animationVar = column_7_Counter;};
 
+// Remove the event-listener for this function on the played cell so it's not possible to invoke 
+// the it again and again by clicking often (what causes errors & bugs because of the timeout function)
 
+topCell.removeEventListener("click", GameFlow);
 
-}); // Ende Event Listener
+// Creat the coin and set correct position
 
-}; // Ende Array
+let coin = document.createElement("img");
+coin.id = "ID_Coin";
+if(playerIsOnTurn === "left")coin.src = "./Folder_Graphics/freescg_org/diamond_Blue.svg"
+else coin.src = "./Folder_Graphics/freescg_org/diamond_Violett.svg";
+coin.style.height = "3rem";
+coin.style.width = "3rem";
 
-function placingCoin(cellID){
-    document.getElementById(`${cellID}`).classList.add("Class_PlacingTransition");
-    }
+// If  there is no coin appended to the cell, add the coin &  the correct animation (animation length)
 
-function animater(cell, columnNumber){
-    // Inrease the row counter to count the columns played in this row
-    if(columnNumber == 1)column_1_Counter++;
+if(!cell.firstChild){ 
+coin.classList.add(`Class_PlacingAnimation_Cell_${animationVar}`);
+cell.appendChild(coin);
+}
 
+// After the animation to the last free column finished (giving 1 second time), 
+// remove the coin from the top cell, remove the Choosing Animation and place the Coin as background image 
+// on the correct column (with the counter decrasing with each coin)
 
-    // Creat the coin and set correct position
-    let coin = document.createElement("img");
-    coin.id = "coin";
-    coin.src = "./Folder_Graphics/X_es/handle-x-svgrepo-com.svg";
-    coin.style.height = "2rem";
-    coin.style.width = "2rem";
-    if(!cell.firstChild){ 
-    coin.classList.add("Class_PlacingAnimation");
-    
-    cell.appendChild(coin);
-    }
-    } 
-        
-
-
-
-
-    /*
 setTimeout(()=>{
-topCell.removeChild(topCell.firstChild);
-animater(cell_2);
-}, 700);
-    setTimeout(()=>{
-        cell_2.removeChild(cell_2.firstChild);
-        animater(cell_3);
-        }, 1400);
-            setTimeout(()=>{
-            cell_3.removeChild(cell_3.firstChild);
-            animater(cell_4);
-            }, 2100);*/
+    cell.firstChild.remove()
+    coin.classList.remove("Class_PlacingAnimation");
+    if(playerIsOnTurn === "left") document.getElementById(`ID_C${columnNumber}R${animationVar}`).classList.add("Class_PlacedCoin_1")
+    else document.getElementById(`ID_C${columnNumber}R${animationVar}`).classList.add("Class_PlacedCoin_2");
+
+    // If placement is finished, ad the event listener with gameflow back again: Next placement is possible
+
+    topCell.addEventListener("click", GameFlow);
+
+}, 1000); // End of the setTimeout function
+
+}; // End of the Main Game function
+
+}); // End Event-Listener with main Game-Function
+
+}; // End Main Game For-Loop
+
+
+// Jobs:
+// Game settings like names ...
+// Player change placements
+// Styling 
+// Code improvement
+// Mobile & Responisve if possible
+// Baue einen zähler ein bei gerade darf nu rrot undbei ungerde nur blau damit eine zweite schleife drinne ist---
+// Win Validation
+// Win div mit Ellypse positon absolute über wim row
+
+
+// Ternäre operatore x>100 ? "x größer als hundert" : "nicht größer"
+// Conditonales setzen einer Variablen statt if  const user = user_name || "Player 1"
+// Number to string: let int = "14"; neueZahl = +int / umgekehrt zu string: const zahlString = 5 + ""; in einer concntatiom zwei tilde ~~
+// In array alle strings zu nummer = values = array.map(Number); Bollean: array.map(Boolean)
+// 2 << 3 = 16 ist gleich wie 2 ** 4 = 16 oder old style Math.pow(2, 3)
+// float to int mit zahl | 0 = rundet auf wenn negativ und ab wen negativ. get auch mit doppelt tilde so
+// |0 rundet ja eine positive float auf eine int ab. also 1222 / 10 | 0 --> 1220 / 10 = 122,0 | 0 = 122
+// Destructing von objekten oder arrays: const {name, age, ...} = user --> und die variablen haben die werte vo m user objekt
+// Console.time("loop")  for loop console.timeEnd("loop") gibt den loop aus
+// slice kann auch negative values und damit bekommt man di eletzten values einen arrays
+
+/*####################################################################################################################################################################*\
+#                                                                                                                                                                      #
+#                                                                     Credits & Special Thanks to:                                                                                 #
+#                                                                                                                                                                      #
+#                                                        Special thanks to the "Odin Project"-Team who did a great job.                                                #
+#                                                    Greetings to the many, many programmers who take the time to write blogs,                                         #
+#                                      Of course also big thanks to all photographers and graphic designers who make their works available.                            #
+#                                                                                                                                                                      #
+#                                                                    CSS - what a wonderful language.                                                                  #
+#                                                                                                                                                                      #
+#                                                                                                                                                                      #
+\*####################################################################################################################################################################*/
---
+++ README.md
@@ -0,0 +1,16 @@
+# Four Wins / Vier Gewinnt
+This is a project by Stefan Bartl from Vienna, Austria.
+
+It is made with Visual Studio Code, Chrome/Firefox, Git and GitHub on a Linux Ubuntu OS.
+Thanks to all of the people which made that or working there.
+
+Also credits to:
+
+A few words to the readers:
+The code is - as you can see - sufficiently commented. For some programmers this might seem strange, but for me and my way of coding it is extremely helpful. I like to use whitespace for formatting,  all classes start with Class_ and all id's with ID_  - and, i also describe the individual processes with comments in as much detail as possible so that I can get back to work reliably and quickly after a while. This makes working on projects for me a lot easier. I hope that this approach is also helpful for you.
+
+Specially about this work:
+
+
+Assignment:
+
