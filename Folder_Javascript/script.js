7ae5a76b35348d2cdf492bda0a24e1a51d10291a
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-02-22 13:25:57 +0100

    Update script.js

--- Folder_Javascript/script.js
+++ Folder_Javascript/script.js
@@ -1,4 +1,5 @@
                                                                                                                                                                                                                                                                                     /*
+
                                              Four-Wins-Online Main-Javascript-File                                                           
                                                            powered by
                             
